1c9354929f8c9b6eae6a441586c0fbca1453be8e
Angela <angela.kuzmina@gmail.com>
2024-08-12 18:42:36 +0300

    add fiel

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -1,159 +1,94 @@
 public class Lesson_3 {
-   public static void main(String[] args) {
-        // Создаем животных
-        Dog bobik = new Dog("Бобик");
-        Cat barsik = new Cat("Барсик");
+    static class Animal {
+        String name;
+        int distanceRunLimit;
+        int distanceSwimLimit;
+        boolean isHungry = true;
 
-        // Проверяем действия животных
-        bobik.run(150);
-        bobik.swim(10);
-        barsik.run(200);
-        barsik.swim(1);
-
-        // Создаем массив котов и миску с едой
-        Cat[] cats = {new Cat("Мурка"), new Cat("Васька"), new Cat("Пушок")};
-        FoodBowl bowl = new FoodBowl(10);
-
-        // Коты пытаются покушать
-        for (Cat cat : cats) {
-            cat.eat(bowl);
+        public Animal(String name) {
+            this.name = name;
         }
 
-        // Выводим информацию о сытости котов
-        for (Cat cat : cats) {
-            System.out.println(cat.getName() + " сыт: " + cat.isHungry());
+        public void run(int distance) {
+            if (distance <= distanceRunLimit) {
+                System.out.println("Животное пробежало " + distance + " м.");
+            } else {
+                System.out.println("Животное не смогло пробежать такое расстояние");
+            }
         }
 
-        // Добавляем еду в миску
-        bowl.addFood(5);
-        System.out.println("В миске теперь " + bowl.getFoodAmount() + " еды.");
-    }
-}
+        public void swim(int distance) {
+            if (distance <= distanceSwimLimit) {
+                System.out.println("Животное проплыло " + distance + " м.");
+            } else {
+                System.out.println("Животное не смогло проплыть такое расстояние");
+            }
+        }
 
-class Animal {
-    private String name;
-    private static int animalCount = 0;
-
-    public Animal(String name) {
-        this.name = name;
-        animalCount++;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void run(int distance) {
-        if (distance <= getMaxRunDistance()) {
-            System.out.println(name + " пробежал " + distance + " м.");
-        } else {
-            System.out.println(name + " не может пробежать " + distance + " м.");
+        public  void eat(int foodAmount) {
+            if (foodAmount >= 0) {
+                isHungry = false;
+                System.out.println("Еды хватило, животное наелось");
+            } else {
+                System.out.println("Еды не хватило (");
+            }
         }
     }
 
-    public void swim(int distance) {
-        if (distance <= getMaxSwimDistance()) {
-            System.out.println(name + " проплыл " + distance + " м.");
-        } else {
-            System.out.println(name + " не может проплыть " + distance + " м.");
+    static class Cat extends Animal {
+        Cat(String name) {
+            super(name);
+            distanceRunLimit = 200;
+            distanceSwimLimit = 0;
         }
     }
 
-    protected int getMaxRunDistance() {
-        return 0; // Базовый класс не может бежать
-    }
-
-    protected int getMaxSwimDistance() {
-        return 0; // Базовый класс не может плавать
-    }
-
-    public static int getAnimalCount() {
-        return animalCount;
-    }
-}
-
-class Dog extends Animal {
-    private static int dogCount = 0;
-
-    public Dog(String name) {
-        super(name);
-        dogCount++;
-    }
-
-    @Override
-    protected int getMaxRunDistance() {
-        return 500;
-    }
-
-    @Override
-    protected int getMaxSwimDistance() {
-        return 10;
-    }
-
-    public static int getDogCount() {
-        return dogCount;
-    }
-}
-
-class Cat extends Animal {
-    private static int catCount = 0;
-    private boolean hungry = true;
-
-    public Cat(String name) {
-        super(name);
-        catCount++;
-    }
-
-    @Override
-    protected int getMaxRunDistance() {
-        return 200;
-    }
-
-    @Override
-    protected int getMaxSwimDistance() {
-        return 0; // Коты не умеют плавать
-    }
-
-    public void eat(FoodBowl bowl) {
-        if (bowl.getFoodAmount() >= 1 && hungry) {
-            bowl.eatFood(1);
-            hungry = false;
-            System.out.println(getName() + " покушал из миски.");
-        } else {
-            System.out.println(getName() + " не стал есть.");
+    static class Dog extends Animal {
+        Dog(String name) {
+            super(name);
+            distanceRunLimit = 500;
+            distanceSwimLimit = 10;
         }
     }
 
-    public boolean isHungry() {
-        return hungry;
-    }
+    static class Bowl {
+        int foodAmount;
 
-    public static int getCatCount() {
-        return catCount;
-    }
-}
+        public void addFood(int amount) {
+            foodAmount += amount;
+        }
 
-class FoodBowl {
-    private int foodAmount;
-
-    public FoodBowl(int foodAmount) {
-        this.foodAmount = foodAmount;
-    }
-
-    public int getFoodAmount() {
-        return foodAmount;
-    }
-
-    public void eatFood(int amount) {
-        if (amount <= foodAmount) {
-            foodAmount -= amount;
-        } else {
-            System.out.println("В миске не хватает еды.");
+        public int getFoodAmount() {
+            return foodAmount;
         }
     }
 
-    public void addFood(int amount) {
-        foodAmount += amount;
-    }
 
+        public static void main(String[] args) {
+            Bowl bowl = new Bowl();
+            bowl.addFood(20);
+
+            Cat cat1 = new Cat("Масяня");
+            Cat cat2 = new Cat("Царапка");
+            Dog dog = new Dog("Шарик");
+
+            System.out.println("Первое животное: " + cat1.name);
+            cat1.run(160);
+            cat1.swim(150);
+
+            System.out.println("Второе животное: " + cat2.name);
+            cat2.run(300);
+            cat2.swim(0);
+
+            System.out.println("Третье животное: " + dog.name);
+            dog.run(30);
+            dog.swim(356);
+
+            Animal[] animals = {cat1, cat2, dog};
+
+            for (Animal animal : animals) {
+                animal.eat(bowl.getFoodAmount());
+                System.out.println("Животное с именем " + animal.name +  " голодно: " + animal.isHungry);
+        }
+    }
 }
