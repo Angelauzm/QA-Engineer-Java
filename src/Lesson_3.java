182a60b5e0a82bacbc73572bef93a6d2f16be715
Angela <angela.kuzmina@gmail.com>
2024-08-12 12:25:06 +0300

    add files cat

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -1,5 +1,5 @@
 public class Lesson_3 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         // Создаем животных
         Dog bobik = new Dog("Бобик");
         Cat barsik = new Cat("Барсик");
