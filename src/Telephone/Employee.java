e4f98edc29bda206b8b6b12a0a0dbd54862f2d76
Angela <angela.kuzmina@gmail.com>
2024-08-19 17:32:00 +0300

    add files lesson_5

--- src/Telephone/Employee.java
+++ src/Telephone/Employee.java
@@ -1,2 +1,31 @@
-package Telephone;public class Employee {
+package Telephone;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Employee(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ": " + phoneNumber;
+    }
 }
+
--- src/Telephone/Main.java
+++ src/Telephone/Main.java
@@ -1,2 +1,32 @@
-package tel;public class Main {
+package Telephone;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        PhoneBook book = new PhoneBook();
+
+        // Добавление записей
+        book.add("Иванов", "Иван", "123-456-7890");
+        book.add("Петров", "Петр", "987-654-3210");
+        book.add("Петров", "Петр", "456-456-345-234");
+        book.add("Кузнецова", "Анна", "555-123-4567");
+
+        List<Employee> employee1 = book.get("Иванов");
+        System.out.println("Телефоны сотрудника Иванова");
+        for (Employee employee : employee1) {
+            System.out.println(employee);
+        }
+
+        List<Employee> employee2 = book.get("Кузнецова");
+        System.out.println("\nТелефоны сотрудника Кузнецовой:");
+        for (Employee employee : employee2) {
+            System.out.println(employee);
+        }
+
+        List<Employee> employee3 = book.get("Петров");
+        System.out.println("\nТелефоны сотрудника Петрова");
+        for (Employee employee : employee3) {
+            System.out.println(employee);
+        }
+    }
 }
